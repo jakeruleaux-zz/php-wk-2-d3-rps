e1465588a327b4ef43c2f92968a0678d7885bff9
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-05 20:53:02 -0700

    lay-out templates for model and test files

---
+++ src/RockPaperScissors.php
@@ -0,0 +1,29 @@
+<?php
+
+      class RockPaperScissors
+
+      {
+
+          private = $first_input;
+          private = $second_input;
+
+          function __construct($new_first_input, $new_second_input)
+
+          {
+            $this->first_input = $new_first_input;
+            $this->second_input = $new_second_input;
+          }
+
+          function getFirstInput()
+
+          {
+            return $this->first_input;
+          }
+
+          function getSecondInput()
+          {
+            return $this->second_input;
+          }
+      }
+
+ ?>
