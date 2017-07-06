1101effb97f7c3bdd80b281cc6506959ca6f9538
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-05 22:06:17 -0700

    make final test

--- test/test:RockPaperScissorsTest.php
+++ test/test:RockPaperScissorsTest.php
@@ -46,6 +46,62 @@
             //Assert
             $this->assertEquals("Draw", $result);
         }
+
+        function testPaperScissors()
+        {
+            //Arrange
+            $test_rock_paper_scissors = new RockPaperScissors;
+            $first_input = "paper";
+            $second_input = "scissors";
+
+            //Act
+            $result = $test_rock_paper_scissors->playGame($first_input, $second_input);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+
+        function testRockPaper()
+        {
+            //Arrange
+            $test_rock_paper_scissors = new RockPaperScissors;
+            $first_input = "rock";
+            $second_input = "paper";
+
+            //Act
+            $result = $test_rock_paper_scissors->playGame($first_input, $second_input);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+
+        function testPaperRock()
+        {
+            //Arrange
+            $test_rock_paper_scissors = new RockPaperScissors;
+            $first_input = "paper";
+            $second_input = "rock";
+
+            //Act
+            $result = $test_rock_paper_scissors->playGame($first_input, $second_input);
+
+            //Assert
+            $this->assertEquals("Player 1", $result);
+        }
+
+        function testScissorsPaper()
+        {
+            //Arrange
+            $test_rock_paper_scissors = new RockPaperScissors;
+            $first_input = "scissors";
+            $second_input = "paper";
+
+            //Act
+            $result = $test_rock_paper_scissors->playGame($first_input, $second_input);
+
+            //Assert
+            $this->assertEquals("Player 1", $result);
+        }
     }
 
 ?>
